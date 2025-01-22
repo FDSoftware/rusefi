d5153f05ca4af97b1648ccdb3d5c0f3c943a4905
FDSoftware <diego+gh@churrosoft.ar>
2025-01-22 00:34:06 -0300

    add alpha-x paralela fw to main repo

---
+++ firmware/config/boards/alpha-x-paralela/board.mk
@@ -0,0 +1,23 @@
+# defines SHORT_BOARD_NAME
+include $(BOARD_DIR)/meta-info.env
+
+# one day when we are grown ups and can coordinate a real life test we shall revisit https://github.com/rusefi/rusefi/issues/6008
+DDEFS += -DDISABLE_PIN_STATE_VALIDATION=TRUE
+
+# reduce memory usage monitoring
+DDEFS += -DRAM_UNUSED_SIZE=100
+
+# assign critical LED to a non-existent pin
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+
+# MAJOR HACK? SOMETHING IS FUNNY HERE?!
+DDEFS += -DDISABLE_PIN_STATE_VALIDATION=TRUE
+
+# temporary or not? (pps noise stuff)
+DDEFS += -DETB_INTERMITTENT_LIMIT=60001
+
+DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
+# todo: make knock pin software-selectable?
+# todo: DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE
+
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
